55e750630343c0191993fb38a7e6c81e1a26bf76
BDStanley <bdstanley@gmail.com>
2019-03-21 19:45:45 +0100

    gg

--- National polls.R
+++ National polls.R
@@ -8,7 +8,7 @@ library(maptools); library(rgeos); library(gpclib); library(reshape2); library(p
 library(gridExtra); library(grid); library(cowplot); library(scales)
 gpclibPermit()
 
-#text change now
+#text change now here
 
 # d'Hondt function
 countN <- function (v) {return (Reduce(function (x, y) x + y, ifelse(is.na(v), 0, 1)))}
