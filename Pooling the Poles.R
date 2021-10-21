8d94294ba076a3a94cb21b24e5e546a7c125496b
BDStanley <bdstanley@gmail.com>
2021-10-21 12:05:24 +0200

    21 oct

--- Pooling the Poles.R
+++ Pooling the Poles.R
@@ -1650,13 +1650,13 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #                   ylim = c(0, .5)) +
 #   scale_color_manual(values=cols) +
 #   scale_fill_manual(values=cols, guide=FALSE) +
-#   labs(y = "", x="", title = "Trendy", 
+#   labs(y = "", x="", title = "Trendy",
 #        subtitle=str_to_upper(str_c("Dane: ", names, ".")), color="", caption = "Ben Stanley (@BDStanley; benstanley.pl).") +
 #   theme_minimal() +
 #   theme_ipsum_rc() +
 #   guides(colour = guide_legend(override.aes = list(alpha = 1, fill=NA))) +
 #   theme_changes
-# ggsave(plot_trends_parl, file = "plot_trends_parl_PL.png", 
+# ggsave(plot_trends_parl, file = "plot_trends_parl_PL.png",
 #        width = 7, height = 5, units = "cm", dpi = 320, scale = 4, bg="white")
 # 
 # plotdraws <- add_fitted_draws(
@@ -1734,7 +1734,7 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #                             labels = c("PiS", "KO", "Lewica", "Konfederacja", "Inni", "PSL","Polska 2050"))) %>%
 #   ggplot() +
 #   geom_vline(aes(xintercept=0.05), colour="gray40", linetype="dotted") +
-#   stat_slab(aes(y=reorder(.category, dplyr::desc(-.value)), 
+#   stat_slab(aes(y=reorder(.category, dplyr::desc(-.value)),
 #                 x=.value, fill=.category), normalize="xy") +
 #   scale_y_discrete(name="", position="right") +
 #   annotate(geom = "text", label=paste(round(medians$est[medians$.category=="PiS"],0)),
@@ -1775,8 +1775,8 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #        subtitle=str_to_upper(str_c("Dane: ", names,"."))) +
 #   theme_minimal() +
 #   theme_ipsum_rc() +
-#   theme_changes 
-# ggsave(plot_latest_parl, file = "polls_latest_parl_PL.png", 
+#   theme_changes
+# ggsave(plot_latest_parl, file = "polls_latest_parl_PL.png",
 #        width = 7, height = 5, units = "cm", dpi = 320, scale = 4, bg="white")
 # 
 # plot_seats_parl <- ggplot(data=frame, mapping=aes(x=party, y=y, fill=party)) +
@@ -1800,21 +1800,21 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 # ggsave(plot_seats_parl, file = "plot_seats_parl_PL.png",
 #        width = 7, height = 5, units = "cm", dpi = 320, scale = 4, bg="white")
 
-# #####INCLUDING DON'T KNOWS#####
+#####INCLUDING DON'T KNOWS#####
 # polls <- read_excel('polldata.xlsx')
-# 
+#
 # polls <- unite(polls, org, remark, col="org", sep="_") %>%
 #   filter(., DK!=0)
 # polls$org <-as.factor(polls$org)
-# 
+#
 # polls$startDate <- as.Date(polls$startDate)
 # polls$endDate <- as.Date(polls$endDate)
 # polls <- unite(polls, org, remark, col="org", sep="_")
 # polls$org <-as.factor(polls$org)
-# 
+#
 # polls$startDate <- as.Date(polls$startDate)
 # polls$endDate <- as.Date(polls$endDate)
-# 
+#
 # polls <-
 #   polls %>%
 #   mutate(midDate = as.Date(startDate + (difftime(endDate, startDate)/2)),
@@ -1825,8 +1825,8 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #          Polska2050 = `Polska 2050`,
 #          time = as.integer(difftime(midDate, min(midDate), units = "days")),
 #          pollster = as.integer(factor(org)))
-# 
-# cols <- c("PiS"="blue4", "KO"="orange", "PSL"="darkgreen", "Konfederacja" = "midnightblue", "Lewica" = "red", 
+#
+# cols <- c("PiS"="blue4", "KO"="orange", "PSL"="darkgreen", "Konfederacja" = "midnightblue", "Lewica" = "red",
 #           "MN" = "yellow", "Other"="gray50", "Polska 2050"="darkgoldenrod", "Don't know" = "gray50")
 # names <- data.frame(as.factor(get_labels(polls$org)))
 # names <- separate(names, as.factor.get_labels.polls.org.., c("house", "method"), sep="_")
@@ -1835,25 +1835,25 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #   fct_collapse(., Kantar=c("Kantar"))
 # names <- glue_collapse(get_labels(housenames), ", ", last = " and ")
 # polls$org <- str_replace_all(polls$org, "_", ", ")
-# 
+#
 # polls <-
 #   polls %>%
 #   mutate(time = interval(min(midDate), midDate)/years(1))
-# 
+#
 # polls[names(polls) %in% c("PiS", "KO", "Lewica", "PSL", "Konfederacja", "Polska2050", "DK")] <-
 #   polls[names(polls) %in% c("PiS", "KO", "Lewica", "PSL", "Konfederacja", "Polska2050", "DK")] %>%
 #   mutate_all(function(x) (as.numeric(str_remove(x, "%"))/100)-0.001) #to ensure no zeroes in model
-# 
+#
 # polls <-
 #   polls %>%
 #   mutate(DK = 1 - c(PiS + KO + Lewica + PSL + Konfederacja + Polska2050))
-# 
+#
 # polls <-
 #   polls %>%
 #   mutate(
 #     outcome = as.matrix(polls[names(polls) %in% c("PiS", "KO", "Lewica", "PSL", "Konfederacja", "Polska2050", "DK")])
 #   )
-# 
+#
 # m1 <-
 #   brm(formula = bf(outcome ~ 1 + s(time, k = 10) + (1 | pollster)),
 #       family = dirichlet(link = "logit", refcat = "PSL"),
@@ -1895,16 +1895,15 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #           max_treedepth = 15
 #         )
 #   )
-# 
-# #####Trend plot#####
+#
 # today <- interval(min(polls$midDate), Sys.Date())/years(1)
-# 
+#
 # pred_dta <-
 #   tibble(
 #     time = seq(0, today, length.out = nrow(polls)),
 #     date = as.Date(time*365, origin = min(polls$midDate))
 #   )
-# 
+#
 # pred_dta <-
 #   add_fitted_draws(
 #     model = m1,
@@ -1921,7 +1920,7 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #           labels = c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")
 #       )
 #   )
-# 
+#
 # point_dta <-
 #   polls[names(polls) %in% c("midDate", "PiS", "KO", "Lewica", "Konfederacja", "DK", "PSL", "Polska2050")] %>%
 #   pivot_longer(
@@ -1937,12 +1936,12 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #         labels = c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")
 #       )
 #   )
-# 
+#
 # plot_trends_parl_DK <-
 #   ggplot() +
-#   geom_point(data=point_dta %>% filter(., party %in% c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")), 
+#   geom_point(data=point_dta %>% filter(., party %in% c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")),
 #              aes(x = midDate, y = est, colour = party, fill = party), alpha = .5, size = 1, show.legend=FALSE) +
-#   stat_lineribbon(data=pred_dta %>% filter(., party %in% c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")), 
+#   stat_lineribbon(data=pred_dta %>% filter(., party %in% c("PiS", "KO", "Polska 2050", "Lewica", "Konfederacja", "PSL", "Don't know")),
 #                   aes(x = date, y = .value, color=party, fill=party), .width=c(0.5, 0.66, 0.95), alpha=1/4) +
 #   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
 #   scale_x_date(date_breaks = "1 month",
@@ -1951,13 +1950,13 @@ ggsave(plot_trends_pollster , file = "plot_trends_pollster.png",
 #                   ylim = c(0, .5)) +
 #   scale_color_manual(values=cols) +
 #   scale_fill_manual(values=cols, guide=FALSE) +
-#   labs(y = "% of vote", x="", title = "Trends (including undecided voters)", 
+#   labs(y = "% of vote", x="", title = "Trends (including undecided voters)",
 #        subtitle=str_c("Data from ", names, "."), color="", caption = "Ben Stanley (@BDStanley; benstanley.pl).") +
 #   theme_minimal() +
 #   theme_ipsum_rc() +
 #   guides(colour = guide_legend(override.aes = list(alpha = 1, fill=NA))) +
 #   theme_changes
-# ggsave(plot_trends_parl_DK, file = "plot_trends_parl_DK.png", 
+# ggsave(plot_trends_parl_DK, file = "plot_trends_parl_DK.png",
 #        width = 7, height = 5, units = "cm", dpi = 320, scale = 4, bg="white")
 
 
