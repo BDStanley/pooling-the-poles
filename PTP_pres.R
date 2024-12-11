5a3877ecbbecda44a4e2ca80c8f7a5052d8a8d1e
BDStanley <bdstanley@gmail.com>
2024-12-11 20:46:50 +0100

    PTP new

--- PTP_pres.R
+++ PTP_pres.R
@@ -24,14 +24,14 @@ theme_plots <- function() {
 # update_geom_defaults("label_repel", 
 #                      list(family = "IBM Plex Sans Condensed"))
 
-theme_pandem <- function(base_size = 11, base_family = "Noto Sans", prior = FALSE) {
+theme_plots <- function(base_size = 11, base_family = "Fira Sans", prior = FALSE) {
   ret <- theme_bw(base_size, base_family) +
     theme(panel.background = element_rect(fill = "#ffffff", colour = NA),
-          title = element_text(size = rel(1), family = "Noto Sans", face = "bold"),
+          title = element_text(size = rel(1), family = "Fira Sans", face = "bold"),
           plot.subtitle = element_text(size = rel(0.8),
-                                       family = "Noto Sans", face = "plain"),
+                                       family = "Fira Sans", face = "plain"),
           plot.caption = element_text(margin = margin(t = 10), size = rel(0.6),
-                                      family = "Noto Sans", face = "plain"),
+                                      family = "Fira Sans", face = "plain"),
           panel.border = element_rect(color = "grey50", fill = NA, linewidth = 0.15),
           panel.spacing = unit(1, "lines"),
           panel.grid.minor = element_blank(),
@@ -39,19 +39,19 @@ theme_pandem <- function(base_size = 11, base_family = "Noto Sans", prior = FALS
           axis.line = element_blank(),
           axis.ticks = element_blank(),
           axis.title = element_text(size = rel(0.8),
-                                    family = "Noto Sans", face = "bold"),
+                                    family = "Fira Sans", face = "bold"),
           axis.title.x = element_text(hjust = 0, margin = margin(t = 10)),
           axis.title.y = element_text(hjust = 1, margin = margin(r = 10)),
           legend.position = "bottom",
           legend.title = element_text(size = rel(0.8), vjust = 0.5,
-                                      family = "Noto Sans", face = "bold"),
+                                      family = "Fira Sans", face = "bold"),
           legend.key.size = unit(0.7, "line"),
           legend.key = element_blank(),
           legend.spacing = unit(0.1, "lines"),
           legend.justification = "left",
           legend.margin = margin(t = -5, b = 0, l = 0, r = 0),
           strip.text = element_text(size = rel(0.9), hjust = 0,
-                                    family = "Noto Sans", face = "bold"),
+                                    family = "Fira Sans", face = "bold"),
           strip.background = element_rect(fill = "white", colour = NA))
   
   if (prior) {
